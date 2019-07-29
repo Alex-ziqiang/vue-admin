265b8e3bed3154843f6925e3fb6f888b49948e89
陈琪 <chenqi1@cmiot.chinamobile.com>
2019-07-29 11:49:41 +0800

    菜单

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Layout from '@/layout/index'
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/login/index'),
-      hidden: true
+export const constantRoutes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/home/index')
+  },
+  {
+    path: '',
+    component: Layout,
+    redirect: '/index',
+    meta: {
+      title: '主页',
+      icon: 'iconfont icon-home'
     },
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/home/index')
-    }
-  ]
+    children: [
+      {
+        path: 'index',
+        name: 'index',
+        component: () => import('@/views/home/index')
+      }
+    ]
+  }
+]
+const createRouter = () => new Router({
+  mode: 'history',
+  routes: constantRoutes
 })
+const router = createRouter()
+export default router
