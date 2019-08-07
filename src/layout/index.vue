9a14315f30d3a03c5ba4eb04ce2c90573137a844
qige2016 <286882998@qq.com>
2019-08-07 08:13:05 +0800

    fix:el-table width bug

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -27,12 +27,13 @@ export default {
   height: 100%;
   width: 100%;
   .main {
-    height: 100%;
+    height: calc(100% - 62px);
     flex: auto; // div.main 占满div.row除了header的额外空间
     display: flex;
     .app-main {
       flex: auto;
       flex-direction: column;
+      overflow-x: hidden; // 解决flex布局下element-ui table 宽度不能自适应问题
     }
   }
 }
