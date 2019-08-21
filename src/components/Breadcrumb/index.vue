e89e52a2ed64db31ff0d7968d3fd23ce257cc189
qige2016 <286882998@qq.com>
2019-08-21 10:46:15 +0800

    fix: Breadcrumb

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="breadcrumb-wrapper">
     <el-breadcrumb separator-class="el-icon-arrow-right">
-      <template v-for="item in $route.matched">
-        <el-breadcrumb-item
-          v-if="item.path"
-          :key="item.path"
-        >
-          {{ item.meta.title }}
-        </el-breadcrumb-item>
-      </template>
+      <el-breadcrumb-item
+        v-for="item in $route.matched"
+        :key="item.path"
+      >
+        {{ item.meta.title }}
+      </el-breadcrumb-item>
     </el-breadcrumb>
   </div>
 </template>
